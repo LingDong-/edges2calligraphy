e7ba1bbacf49defb5513dbdfc413792fa6333bcb
Lingdong Huang <lingdong0618@hotmail.com>
2018-04-03 11:44:36 -0400

    Add pics

--- edges2calligraphy/main.js
+++ edges2calligraphy/main.js
@@ -792,6 +792,40 @@ function downloadContext(ctx){
 
 makeBG({col:[0.145,0.15,0.15],tex:30,spr:0})
 
+function check_browser_compatibility(){
+  // Opera 8.0+
+  var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+  // Firefox 1.0+
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+
+  // Safari 3.0+ "[object HTMLElementConstructor]" 
+  var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+  // Internet Explorer 6-11
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+  // Edge 20+
+  var isEdge = !isIE && !!window.StyleMedia;
+
+  // Chrome 1+
+  var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+  // Blink engine detection
+  var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+  if (isFirefox || isChrome){
+    return true
+  }
+  if (isSafari){
+    window.alert("This demo does not work in Safari. We recommend Chrome or Firefox.")
+  }else{
+    window.alert("This demo is not tested for your browser. We recommend Chrome or Firefox.") 
+  }
+}
+
+check_browser_compatibility()
+
 preload().then(()=>{
   load().then(()=>{
     console.log("ready")
